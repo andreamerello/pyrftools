88dc17c6a6cb45ee5ff5962c68e0df3a2d30207f
Andrea Merello <andrea.merello@gmail.com>
2015-12-08 17:13:56 +0100

    microstrip_L: make it take input params from command line

--- microstrip_L.py
+++ microstrip_L.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
+import sys
 from math import log
 from measure import parse_length
-
-width_str = "2.5mm"
-length_str = "71.5mm"
-distance_str = "10mm"
 
 def calc_L(width_in, distance_in, length_in):
     w = width_in
@@ -13,18 +10,22 @@ def calc_L(width_in, distance_in, length_in):
     # formula from ARRL handbook. NB log is ln
     return 0.00508 * b * (log(2 * b / (w + h)) + 0.5 + 0.2235 * (w + h) / b)
 
-width_in, width_m = parse_length(width_str, 'width')
-length_in, length_m = parse_length(length_str, 'length')
-distance_in, distance_m = parse_length(distance_str, 'distance')
-
-print "Width: {:f} m, ({:f} in)".format(width_m, width_in)
-print "Length: {:f} m, ({:f} in)".format(length_m, length_in)
-print "Distance: {:f} m, ({:f} in)".format(distance_m, length_in)
-
-LuH = calc_L(width_in, distance_in, length_in)
-
-if (LuH < 1):
-    LnH = LuH * 1000.0;
-    print "Inductance {:f} nH".format(LnH)
+if (len(sys.argv) != 4):
+    print "Usage:\n{} <track_width> <track_length> <distance>".format(sys.argv[0])
 else:
-    print "Inductance {:f} uH".format(LuH)
+
+    width_in, width_m = parse_length(sys.argv[1], 'width')
+    length_in, length_m = parse_length(sys.argv[2], 'length')
+    distance_in, distance_m = parse_length(sys.argv[3], 'distance')
+
+    print "Width: {:f} m, ({:f} in)".format(width_m, width_in)
+    print "Length: {:f} m, ({:f} in)".format(length_m, length_in)
+    print "Distance: {:f} m, ({:f} in)".format(distance_m, length_in)
+
+    LuH = calc_L(width_in, distance_in, length_in)
+
+    if (LuH < 1):
+        LnH = LuH * 1000.0;
+        print "Inductance {:f} nH".format(LnH)
+    else:
+        print "Inductance {:f} uH".format(LuH)
